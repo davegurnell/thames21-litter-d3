65a45283041f10195299fc536b436e0464e2d1af
Dave Gurnell <dave@untyped.com>
2018-11-20 14:49:03 +0000

    WIP: Stuff

--- main.js
+++ main.js
@@ -12,27 +12,39 @@ var lngMax = -0.234;
 var latMin = 51.48875;
 var latMax = 51.48900;
 
-var json =
-  data.results.filter(function(result) {
-    return result.location != null
+var surveys =
+  rawData.results
+    .filter(function(result) {
+      return result.location != null
         && lng(result) >= lngMin
         && lng(result) <= lngMax
         && lat(result) >= latMin
         && lat(result) <= latMax;
-  });
+    })
+    .sort(function(a, b) {
+      return lng(a) - lng(b);
+    });
 
-var text =
-  JSON.stringify([ json.length, json ], null, 2);
+$("#root").text(JSON.stringify(surveys, null, 2));
 
-$("#root").text(text);
-
-var rectDemo = d3.select("#canvas").
+var canvas = d3.select("#canvas").
   append("svg:svg").
   attr("width", 400).
   attr("height", 300);
 
-rectDemo.append("svg:rect").
-  attr("x", 100).
-  attr("y", 100).
-  attr("height", 100).
-  attr("width", 200);
+canvas
+  .selectAll("g")
+  .data(function() {
+    console.log('AAAA', surveys);
+    return surveys;
+  })
+  .selectAll("circle")
+  .data(function(survey, i) {
+    console.log('BBBB', survey, i);
+    return survey.quadrats;
+  })
+  .enter()
+    .append("circle")
+    .attr("cx", function(d, i, j) { return 50 + 50 * i; })
+    .attr("cy", function(d, i, j) { return 50 + 50 * j; })
+    .attr("r", 2.5);
