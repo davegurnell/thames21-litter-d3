e405d4f2a65aed1df66b883475e7c8251fb57153
Dave Gurnell <dave@untyped.com>
2018-11-20 16:03:58 +0000

    Moar code

--- main.js
+++ main.js
@@ -7,13 +7,25 @@ function lat(result) {
   return result.location._wgs84[1];
 }
 
-var lngMin = -0.236;
-var lngMax = -0.234;
-var latMin = 51.48875;
-var latMax = 51.48900;
+var lngMin = -0.236270;
+var lngMax = -0.233588;
+var latMin = 51.488654;
+var latMax = 51.489636;
 
-var surveys =
-  rawData.results
+var spacing = 20;
+var filename = "thames21_litter.json";
+var litterItem = "sewageMixedWetWipesOrBabyWipes";
+
+function quantity(quadrat, item) {
+  return quadrat.collected
+    .filter(function(coll) { return coll.item === item })
+    .map(function(coll) { return Math.log(coll.quantity); })
+    .reduce(function(a, b) { return a + b; }, 0);
+}
+
+function clean(data) {
+  return data
+    .results
     .filter(function(result) {
       return result.location != null
         && lng(result) >= lngMin
@@ -22,36 +34,35 @@ var surveys =
         && lat(result) <= latMax;
     })
     .sort(function(a, b) {
-      return lng(a) - lng(b);
+      return lng(b) - lng(a);
     });
+}
 
-$("#root").text(JSON.stringify(surveys, null, 2));
+function display(canvas, data) {
+  $("#root").text(JSON.stringify(data, null, 2));
 
-var canvas = d3.select("#canvas").
-  append("svg:svg").
-  attr("width", 500).
-  attr("height", 800);
+  canvas
+    .selectAll("g")
+      .data(data)
+      .enter()
+        .append("g")
+        .attr("transform", function(d, i) {
+          return "translate(" + (spacing + spacing * i) + ")";
+        })
+        .selectAll("circle")
+          .data(function(survey) { return survey.quadrats; })
+          .enter()
+            .append("circle")
+            .attr("cx", function(quadrat, j) { return 0; })
+            .attr("cy", function(quadrat, j) { return spacing + spacing * j; })
+            .attr("r",  function(quadrat) {
+              return quantity(quadrat, litterItem);
+            });
+}
 
-var SPACING = 20;
-var litterItem = "sewageMixedWetWipesOrBabyWipes";
+var canvas = d3.select("#canvas")
+  .append("svg:svg")
+  .attr("width", 500)
+  .attr("height", 800);
 
-canvas
-  .selectAll("g")
-    .data(surveys)
-    .enter()
-      .append("g")
-      .attr("transform", function(d, i) {
-        return "translate(" + (SPACING + SPACING * i) + ")";
-      })
-      .selectAll("circle")
-        .data(function(survey) { return survey.quadrats; })
-        .enter()
-          .append("circle")
-          .attr("cx", function(quadrat, j) { return 0; })
-          .attr("cy", function(quadrat, j) { return SPACING + SPACING * j; })
-          .attr("r",  function(quadrat) {
-            return quadrat.collected
-              .filter(function(coll) { return coll.item === litterItem })
-              .map(function(coll) { return Math.log(coll.quantity); })
-              .reduce(function(a, b) { return a + b; }, 0);
-          });
+display(canvas, clean(rawData));
